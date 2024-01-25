73f04c665a50a3364635e84b2cb6f19ef493b743
NumaanNazir <nq.nqanoon@gmail.com>
2024-01-25 17:09:03 +0530

    Added Arrays and Arange Numpy Function

---
+++ Numpy/NumPy01.py
@@ -0,0 +1,14 @@
+# Numpy arrays and arange function .
+
+#  ARRAYS
+import numpy as np
+x=np.array([0,2,4,6,8,10])
+print(x)
+
+# Arange
+z=np.arange(1,21)
+print(z)
+
+#Arange with Steps
+z_1=np.arange(0,21,2)
+print(z_1)
