abfe46a1788e474b58f00335071310be3080c543
NumaanNazir <nq.nqanoon@gmail.com>
2024-01-26 14:25:17 +0530

    Added Linspace,Diag,Eye,Zeroes,Ones

---
+++ Numpy/NumPy02.py
@@ -0,0 +1,23 @@
+import numpy as np
+#LINSPACE
+
+x=np.linspace(0,1,6)
+print (x)
+#ONES
+
+y=np.ones([3,3])
+print(y)
+#Zeroes
+
+z=np.zeros([2,3])
+print(z)
+
+#Eye
+
+xx=np.eye(3,3)
+print(xx)
+
+#Diagonal
+
+yy=np.diag([1,2,3,4])
+print(yy)
